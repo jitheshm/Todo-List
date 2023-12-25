42c6e2017b1fc9281a8442019a374a1b1b2d6786
jitheshm <jitheshmjithooz@gmail.com>
2023-12-25 21:44:55 +0530

    datas added to dashboard

--- src/components/DashCard.jsx
+++ src/components/DashCard.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-function DashCard() {
+function DashCard({title,count}) {
     return (
         <div className="card p-4 m-3 col-5" style={{ backgroundColor: '#445669' }}>
-            <h5>Tasks Completed</h5>
-            <h1>10</h1>
+            <h4>{title}</h4>
+            <h5>{count}</h5>
         </div>
     )
 }
--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,8 +1,36 @@
+import { useContext } from "react"
 import DashCard from "./DashCard"
 import PieChart from "./PieChart"
+import { TaskContext } from "../App"
 
 
 function Dashboard() {
+    const { task } = useContext(TaskContext)
+    var high = 0, low = 0, completed = 0, pending = 0, upcoming = 0, today = 0
+    let newDate = new Date()
+    let date = newDate.getDate();
+    let month = newDate.toLocaleString('default', { month: 'numeric' });
+    let year = newDate.getFullYear();
+    let curDate = `${year}-${month}-${date}`
+    task.forEach(element => {
+        if (element.priority === "High"&&element.status==="Pending") {
+            high = high + 1
+        }
+        else if(element.priority === "Low"&&element.status==="Pending") {
+            low = low + 1
+        }
+        if (element.status === "Completed") {
+            completed = completed + 1
+        }
+        if (element.date === curDate&&element.status==="Pending") {
+            today = today + 1
+        }else if(new Date(curDate) < new Date(element.date)&&element.status==="Pending"){
+            upcoming=upcoming+1
+        }else if(new Date(curDate) > new Date(element.date)&&element.status==="Pending"){
+            pending=pending+1
+        }
+
+    });
     return (
         <>
             <div className="p-4 ps-5 ms-5 col-12">
@@ -10,12 +38,13 @@ function Dashboard() {
                 <hr />
                 <div className="row" >
                     <div className="col-6 row ">
-                        <DashCard />
-                        <DashCard />
-                        <DashCard />
-                        <DashCard />
-                        <DashCard />
-                        <DashCard />
+
+                        <DashCard title="Today's Task" count={today} />
+                        <DashCard title="Upcoming Tasks" count={upcoming} />
+                        <DashCard title="Pending Tasks" count={pending} />
+                        <DashCard title="Completed Tasks" count={completed} />
+                        <DashCard title="High Priority Tasks" count={high} />
+                        <DashCard title="Low Priority Tasks" count={low} />
 
 
 
@@ -23,14 +52,14 @@ function Dashboard() {
                     <div className="col-6">
                         <div className="my-5" style={{ height: "200px" }} >
 
-                            <PieChart />
+                            <PieChart section="total" value={[today,upcoming,pending,completed]}  />
 
 
                         </div>
 
                         <div className="my-5" style={{ height: "200px" }} >
 
-                            <PieChart /> 
+                            <PieChart section="priority" value={[high,low]}/>
 
 
                         </div>
